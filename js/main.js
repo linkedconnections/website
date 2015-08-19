8713be5a6f0f36955a6ff4536d3c32c23f7ecd32
Pieter Colpaert <pieter.colpaert@okfn.org>
2015-08-19 17:50:24 +0200

    added markers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(function(){
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map',{
+    scrollWheelZoom : false
+  }).setView([51.1, 4.4], 8);
   L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
+  //Add 4 stations on the map that kan be pressed
+  var gentspmarker = L.marker([51.035896, 3.710675]).addTo(map);
+  var brusselcmarker = L.marker([50.845658, 4.356801]).addTo(map);
+  var brugesmarker = L.marker([51.197226, 3.216726]).addTo(map);
+  var antwerpencmarker = L.marker([51.2172, 4.421101]).addTo(map);
+
+  //Scrolling code for the navigation on top
   $('.scroll').on('click', function(event) {
     event.preventDefault();
     var target = "#" + $(this).data('target');
